86f7fadb8b2709491c4bf7b15609b0614ac4bde3
Breno <brenomelogabriel2019@gmail.com>
2025-04-24 13:46:24 -0300

    feat: adding tracking

--- main.py
+++ main.py
@@ -1,40 +1,60 @@
 import cv2
-import numpy as np 
 from ultralytics import YOLO
+from ultralytics import solutions
 
-# Carrega o modelo YOLOv8
-model = YOLO("yolov8n.pt")  # ou "yolov8s.pt" para mais precisão
+model = YOLO("yolov8n.pt")
+cap = cv2.VideoCapture("data/traffic_video.mp4")
 
-# Caminho do vídeo
-video_path = "data/traffic_video.mp4"
-cap = cv2.VideoCapture(video_path)
+intersting_classes = ['motorcycle','car', 'bicycle', 'bus', 'truck', 'traffic light']
 
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
+w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fps = cap.get(cv2.CAP_PROP_FPS)
+
+region_points = [(20, 400), (1080, 400)]  
+
+video_writer = cv2.VideoWriter("output.mp4", 
+                               cv2.VideoWriter_fourcc(*"mp4v"), 
+                               fps, (w, h))
+
+vehicle_counter = 0; 
+
+while True: 
+
+    sucess, frame = cap.read()
+
+    if not sucess: 
         break
 
-    # Faz a inferência
-    results = model(frame)[0]  # Pega o primeiro resultado
+    results = model.track(frame, stream=True, persist=True, tracker="bytetrack.yaml")
 
-    # Percorre as detecções
-    for box in results.boxes:
-        cls_id = int(box.cls[0])              # Classe da detecção
-        conf = float(box.conf[0])             # Confiança
-        label = model.names[cls_id]           # Nome da classe
-        x1, y1, x2, y2 = map(int, box.xyxy[0])  # Coordenadas da bounding box
+    for result in results:
 
-        # Desenha a caixa e o texto
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-        cv2.putText(frame, f"{label} {conf:.2f}", (x1, y1 - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        cls_name = result.names
 
-    # Exibe o frame com as detecções
-    cv2.imshow("Car detector (Pressione ESC para sair)", frame)
+        for box in result.boxes:
 
-    # Sai com ESC
-    if cv2.waitKey(30) & 0xFF == 27:
+            confidence = box.conf
+            id = box.id
+            object_cls = box.id
+
+            cls = int(box.cls[0])
+
+            if cls_name[cls] in intersting_classes:
+
+                [x1, y1, x2, y2] = box.xyxy[0]
+
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
+
+                cv2.putText(frame, f'{cls_name[cls]} {int(box.id.item())}', (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+
+    cv2.imshow("frames", frame)
+
+    if cv2.waitKey(1)&0xFF==27:
         break
-
+        
 cap.release()
+video_writer.release()
 cv2.destroyAllWindows()
